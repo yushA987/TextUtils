872850f1d9bdf4ecbd9856edf032d1f243990555
AyushVerma <114309983+yushA987@users.noreply.github.com>
2024-05-22 10:46:24 +0530

    Update Alert.js

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 function Alert(props) {
   return (
-    props.alert && <div className="alert alert-success d-flex align-items-center" role="alert">
+    <div style={{height:"60px"}}>
+      {props.alert && <div className="alert alert-success d-flex align-items-center" role="alert">
         {/* <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg> */}
-        <div>
-            <strong>{props.alert.type}</strong>: {props.alert.msg}
+        <strong>{props.alert.type}</strong>: {props.alert.msg}
         </div>
+      }
     </div>
+    
   )
 }
 
