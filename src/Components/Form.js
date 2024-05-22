22ec9ae38339797c9ae385b89d25e8b05f026304
AyushVerma <114309983+yushA987@users.noreply.github.com>
2024-05-22 10:59:18 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,13 +4,23 @@ export default function Form(props) {
     const [text, setText] = useState('')
     const toUpperCase = () => {
         let newText = text.toUpperCase();
-        setText(newText);
-        props.showAlert("Converted to upper case", "Success");
+        if(newText == text){
+            props.showAlert("Already in upper case", "Failed");
+        }
+        else{
+            setText(newText);
+            props.showAlert("Converted to upper case", "Success");
+        }
     }
     const toLowerCase = () => {
         let newText = text.toLowerCase();
-        setText(newText);
-        props.showAlert("Converted to lower case", "Success");
+        if(newText == text){
+            props.showAlert("Already in lower case", "Failed");
+        }
+        else{
+            setText(newText);
+            props.showAlert("Converted to lower case", "Success");
+        }
     }
     const handleUpperCase = (event)=>{
         setText(event.target.value);
@@ -29,10 +39,15 @@ export default function Form(props) {
     const HandleExtraSpace = ()=>{
         // let text = document.getElementById("exampleFormControlTextarea1");
         let newStr = text.replace(/\s+/g, ' ');
-        setText(newStr);
-        props.showAlert("Extra spaces removed", "Success");
+        if(newStr == text){
+            props.showAlert("There are no extra spaces", "Failed");   
+        }
+        else{
+            setText(newStr);
+            props.showAlert("Extra spaces removed", "Success");
+        }
     }
-    const AddComa = () =>{
+    const AddComma = () =>{
         let newStr = text.replace(/\s+/g, ',');
         let l = newStr.length - 1;
         while(newStr[l] == ','){
@@ -44,11 +59,11 @@ export default function Form(props) {
         }
         newStr = newStr.substring(start, l+1);
         if(newStr == text){
-            props.showAlert("Coma's already added", "Failed")
+            props.showAlert("Comma's already added", "Failed")
         }
         else{
             setText(newStr);
-            props.showAlert("Coma's Added", "Success");
+            props.showAlert("Commas Added", "Success");
         }
     }
     return (
@@ -62,10 +77,10 @@ export default function Form(props) {
             </div>
             <button disabled={text.length===0} className="btn btn-primary my-3" onClick={toUpperCase}>Convert to Upper Case</button>
             <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>Convert to Lower Case</button>
-            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={AddComma}>Add Commas</button>
             <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={HandleExtraSpace}>Remove Extra Space</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={clrText}>Clear Text</button>
-            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={AddComa}>Add Comas</button>
         </div>
         <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
             <div className="other_details" >
