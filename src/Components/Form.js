75e35119aced18e793b8e026b574f46b97c88920
AyushVerma <114309983+yushA987@users.noreply.github.com>
2024-05-22 10:45:37 +0530

    Update Form.js
    Added extra functionality to the TextUtil webapp.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -16,16 +16,40 @@ export default function Form(props) {
         setText(event.target.value);
     }
     const handleCopy = () =>{
-        let text = document.getElementById("exampleFormControlTextarea1");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        let ntext = document.getElementById("exampleFormControlTextarea1");
+        ntext.select();
+        navigator.clipboard.writeText(text);
+        // or  navigator.clipboard.writeText(ntext.value);
         props.showAlert("Text copied", "Success");
+    }
+    const clrText = ()=>{
+        setText("");
+        props.showAlert("Text cleared", "Success");
     }
     const HandleExtraSpace = ()=>{
         // let text = document.getElementById("exampleFormControlTextarea1");
         let newStr = text.replace(/\s+/g, ' ');
         setText(newStr);
         props.showAlert("Extra spaces removed", "Success");
+    }
+    const AddComa = () =>{
+        let newStr = text.replace(/\s+/g, ',');
+        let l = newStr.length - 1;
+        while(newStr[l] == ','){
+            l--;
+        }
+        let start = 0;
+        while(newStr[start] == ','){
+            start++;
+        }
+        newStr = newStr.substring(start, l+1);
+        if(newStr == text){
+            props.showAlert("Coma's already added", "Failed")
+        }
+        else{
+            setText(newStr);
+            props.showAlert("Coma's Added", "Success");
+        }
     }
     return (
     <>
@@ -36,15 +60,17 @@ export default function Form(props) {
                 onChange={handleUpperCase} style={{backgroundColor: props.mode==='dark'?'#0C2D48':'white',
                 color: props.mode==='dark'?'white':'black'}} rows="8" ></textarea>
             </div>
-            <button className="btn btn-primary my-3" onClick={toUpperCase}>Convert to Upper Case</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>Convert to Lower Case</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={HandleExtraSpace}>Remove Extra Space</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3" onClick={toUpperCase}>Convert to Upper Case</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>Convert to Lower Case</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={HandleExtraSpace}>Remove Extra Space</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={clrText}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={AddComa}>Add Comas</button>
         </div>
         <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
             <div className="other_details" >
                 <h2>Text Summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} characters</p>
             </div>  
         </div>
     </>
