351d82c068a46c5751171612fee67997e6f22d7d
AyushVerma <114309983+yushA987@users.noreply.github.com>
2024-06-21 09:58:47 +0530

    Update App.js with routing functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-// import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar.js'
+import About from './Components/About.js'
+import Contact from './Components/Contact.js'
 import Form from './Components/Form.js'
 import React, {useState} from 'react'
-import Alert from './Components/Alert'
+import Alert from './Components/Alert.js'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 function App() {
+  
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type) =>{
@@ -16,28 +19,43 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-    const toggleMode = ()=>{
-      if(mode === 'light')
+  const toggleMode = ()=>{
+    if(mode === 'light')
       {
         setMode('dark');
         document.body.style.backgroundColor = "#0C2D48";
         showAlert('Dark mode has been enabled','Success');
       }
-    
+      
       else{
         document.body.style.backgroundColor = "white";
         setMode('light');        
         showAlert('Light mode has been enabled','Success');
       }
     }
-  return (
-    <>
-      <Navbar title = "TextUtils" mode = {mode} toggle={toggleMode}/>
-      <Alert alert={alert} />
-      <div className="container">
-        <Form mode = {mode} showAlert={showAlert}/>
-      </div>
-      
+    const router = createBrowserRouter([
+      {
+        path: "/TextUtils",
+        element: <>
+          <Navbar title = "TextUtils" mode = {mode} toggle={toggleMode}/>
+          <Alert alert={alert} />
+          <div className="container">
+            <Form mode = {mode} showAlert={showAlert}/>
+          </div>
+        </>
+      },
+      {
+        path: "/about",
+        element: <><Navbar title = "TextUtils" mode = {mode} toggle={toggleMode}/><About mode = {mode}/></>
+      },
+      {
+        path: "/contact",
+        element: <><Navbar title = "TextUtils" mode = {mode} toggle={toggleMode}/><Alert alert={alert} /><Contact mode = {mode} showAlert={showAlert}/></>
+      }
+    ])
+    return (
+      <>
+      <RouterProvider router = {router}/>
     </>
   );
 }
