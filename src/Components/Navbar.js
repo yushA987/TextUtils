aa437231e825a7eb408ae9ad545ee328841bafb3
AyushVerma <114309983+yushA987@users.noreply.github.com>
2024-06-21 10:31:28 +0530

    Update Navbar.js with routing to about and home page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     <div>
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/TextUtils">{props.title}</NavLink>
+        <NavLink className="navbar-brand" to="/">{props.title}</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
